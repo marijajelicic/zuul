30752e1e782db7d53f9177e1ff7f312c705def97
Marija Jelicic <marija.jelicic@netcast.rs>
2019-11-17 11:39:43 +0100

    -- comments added

--- src/main/java/rs/raf/zuul/ZuulApplication.java
+++ src/main/java/rs/raf/zuul/ZuulApplication.java
@@ -6,7 +6,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+
+//Anotacija kojom aplikaciju pretvaramo u Zuul proxy server
 @EnableZuulProxy
+
+//Anotacija kojom omogucavamo prijavljivanje na Eureka server; @EnableEurekaClient radi samo ako na classpath-u imamo eureka dependecy
+//Opstija anotacja je @EnableDiscoveryClient i radi i za druge service discavery-je (npr.ZooKeeper)
 @EnableEurekaClient
 public class ZuulApplication {
 
